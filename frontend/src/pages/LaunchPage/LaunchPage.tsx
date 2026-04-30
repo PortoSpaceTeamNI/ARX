b847905ea080bb469fc6fa51235bf74685a07149
TomasCacoete <tomaspcacoete@gmail.com>
2026-04-30 15:34:03 +0100

    lint fix

--- frontend/src/pages/LaunchPage/LaunchPage.tsx
+++ frontend/src/pages/LaunchPage/LaunchPage.tsx
@@ -1,11 +1,5 @@
-import Navbar from '@/components/Navbar';
-
 import styles from './LaunchPage.module.scss';
 
 export default function LaunchPage() {
-  return (
-    <main className={styles.launchPage}>
-      <Navbar />
-    </main>
-  );
+  return <main className={styles.launchPage} />;
 }
